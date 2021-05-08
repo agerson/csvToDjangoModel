4d394ac42f441bcffc6a96e76978d63458ae728b
Adam Gerson <adamg496@gmail.com>
2021-05-08 12:01:49 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+from csv import reader
+from email.utils import parseaddr
+
+# open file in read mode
+with open('import.csv', 'r') as read_obj:
+    # pass the file object to reader() to get the reader object
+    csv_reader = reader(read_obj)
+    headers = next(csv_reader)
+    row_1 = next(csv_reader)
+    print(headers)
+    print(row_1)
+
+    model_row = ""
+    for i, col in enumerate(row_1):
+        field_name = headers[i].strip().lower().replace(' ', '_')
+        model_row = field_name + " = models."
+        if col.isnumeric():
+            model_row += "IntegerField()"
+        elif '@' in col:
+            model_row += "EmailField()"
+        else:
+            model_row += "TextField(max_length=100)"
+        print(model_row)
